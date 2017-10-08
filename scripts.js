ef5692296fbbddd7bd44c887ebd62416b8303ae2
Krystian Jabłoński <Krystian Jaboski>
2017-10-08 16:14:41 +0200

    Commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+$(function) {
+    let span = $('span');
+        span.each(function(index, element) {
+            if(index % 2 == 0) {
+                $(element).css('color', 'red');
+            });
+        debugger;
+        };
+    });
+$(function) {
+    let paragraphs = $('p');
+        paragraphs.each(function(index, element){
+            let button = '<button class="btn" data-tmp="' + index + '">Click me</button>' 
+            $('element').append(button)
+        });
+        debugger;
+    $('button').click(function() {
+        alert($(button).attr('data-tmp'));
+    });
+};
