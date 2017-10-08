87fa85b39c3ef7cf057832b1a667ad12768a8242
Krystian Jabłoński <Krystian Jaboski>
2017-10-08 17:14:01 +0200

    fixes

--- scripts.js
+++ scripts.js
@@ -3,18 +3,16 @@ $(function() {
         span.each(function(index, element) {
             if(index % 2 == 0) {
                 $(element).css('color', 'red');
-            });
+            }
         debugger;
-        });
-    });
-$(function() {
+        })
     let paragraphs = $('p');
         paragraphs.each(function(index, element){
             let button = '<button class="btn" data-tmp="' + index + '">Click me</button>' 
             $('element').append(button)
-        });
+        })
         debugger;
     $('button').click(function() {
         alert($(button).attr('data-tmp'));
-    });
+    })
 });
