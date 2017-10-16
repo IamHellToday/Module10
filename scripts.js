3223798879a3ab87f42edd55230fce5f26538a52
Krystian Jabłoński <Krystian Jaboski>
2017-10-16 13:07:47 +0200

    Initial

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-s
+$(function(){
+    let carouselList = $('#carousel ul');
+    function changeSlide(){
+        carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+    }
+    function moveFirstSlide(){
+        let firstItem = carouselList.find('li:first');
+        let lastItem = carouselList.find('li:last');
+            lastItem.after(firstItem);
+            carouselList.css({marginLeft:0});
+    }
+    setInterval(changeSlide, 3000);
+});
